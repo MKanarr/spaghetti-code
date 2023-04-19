31efb6ca5017bf4d48b50a97c5d4dc6463f54d9a
Evamatrix <evelyn.tran@utdallas.edu>
2023-04-18 20:18:07 -0500

    add world

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-using namespace std;
 
-void H(int);
-void e(int);
-void l(int);
-void o(int);
-int main()
-{
-    int arraystuff[] = {72, 101, 108, 111};
-    H(arraystuff[0]);
-    e(arraystuff[1]);
-    l(arraystuff[2]);
-    l(arraystuff[2]);
-    e(arraystuff[3]);
+void print(char world) {
+   int i = 0;  while (filename[i] != '\0') { cout << filename[i]; i++; }
+}
+int main() {
+    char world[] = "World!";
+
+    // my comment
+    std::cout << "Hello UTD" << std::endl;
+
+    print(filename);
     return 0;
 }
-void H(int i) { cout << static_cast<char>(i); }
-void e(int i) { cout << static_cast<char>(i); }
-void l(int i) { cout << static_cast<char>(i); }
-void o(int i) { cout << static_cast<char>(i); }
