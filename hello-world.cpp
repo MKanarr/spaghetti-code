431794a79105a0a6270c8b35090838ad3f063cd1
nikkinikki247 <nicoleni247@gmail.com>
2023-04-18 19:59:35 -0500

    i'mdone-nicole

--- hello-world.cpp
+++ hello-world.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    
-    // my comment
-    std::cout << "Hello UTD" << std::endl;
-
+void H(int);
+void e(int);
+void l(int);
+void o(int);
+int main()
+{
+    int arraystuff[] = {72, 101, 108, 111};
+    H(arraystuff[0]);
+    e(arraystuff[1]);
+    l(arraystuff[2]);
+    l(arraystuff[2]);
+    e(arraystuff[3]);
     return 0;
 }
+void H(int i) { cout << static_cast<char>(i); }
+void e(int i) { cout << static_cast<char>(i); }
+void l(int i) { cout << static_cast<char>(i); }
+void o(int i) { cout << static_cast<char>(i); }
