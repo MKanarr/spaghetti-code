baf16a5e0b1dc5f9557a2bed7b6eac2dfccff84f
Lana <lana.tran0105@gmail.com>
2024-09-18 19:04:45 -0500

    borger

--- hello-world.cpp
+++ hello-world.cpp
@@ -4,6 +4,13 @@ int main() {
     
     // my comment
     std::cout << "Hello UTD" << std::endl;
+    if(true){
+        if(!false){
+            if("cheeseburger" == "cheeseburger") {
+                std::cout << "borger\n";
+            }
+        }
+    }
 
     return 0;
 }
